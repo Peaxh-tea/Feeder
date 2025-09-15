25a739fc50638d197bd3b2f72d835095455b8dc9
AustinArvelo <austina46@nycstudents.net>
2025-09-15 13:10:36 -0400

    added one day cycle

--- Feeder.java
+++ Feeder.java
@@ -17,8 +17,14 @@ public class Feeder {
      * Simulates one day with numBirds birds or possibly a bear at the bird
      * feeder, as described in part (a) Precondition: numBirds > 0
      */
-    public void simulateOneDay(int numBirds)
-    {
+    public void simulateOneDay(int numBirds){
+        if (Math.random() < 0.05) currentFood = 0;
+        else {
+            int eaten = (int) ((Math.random()*41)+10);
+            eaten *= numBirds;
+            currentFood -= eaten;
+            if (currentFood < 0) currentFood = 0;
+        }
     }
 
     /**
@@ -27,6 +33,10 @@ public class Feeder {
      * numBirds > 0, numDays > 0
      */
     public int simulateManyDays(int numBirds, int numDays) {
+        int days = 0;
+        while(numDays > 0) {
+            simulateOneDay(numBirds);
+        }
         return 0;
     }
 
--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Feeder f = new Feeder(500);
+        Feeder f = new Feeder(1000);
         System.out.println(f);
         System.out.println(f.getCurrentFood());
-        System.out.println((int) (Math.random() * 41) + 10);
+        f.simulateOneDay(22);
+        System.out.println(f.getCurrentFood());
     }
 }
